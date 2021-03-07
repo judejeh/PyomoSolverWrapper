606e7dd1c565b6fe72baad4c05b8c64f2b3b5835
Jude E <git@judejeh.com>
2021-03-07 13:40:11 +0000

    Remove 'solvers' folder. Currently unncessary

--- PyoSolveWrapper/Solvers/__init__.py
+++
@@ -1,7 +0,0 @@
-# ---------------------------------------------------------------------------------
-# Pyomo Solver Wrapper
-# Language - Python
-# https://github.com/judejeh/PyomoSolverWrapper
-# Licensed under MIT license
-# Copyright 2021 The Pyomo Solver Wrapper authors <https://github.com/judejeh>
-# ---------------------------------------------------------------------------------
